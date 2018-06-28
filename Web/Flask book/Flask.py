0733df5cbc294202129cff608b9c2f65927aa628
perewodchik <perewodchik@tuta.io>
2018-06-28 20:23:10 +0300

    website

--- Web/Flask book/Flask.py
+++ Web/Flask book/Flask.py
@@ -15,32 +15,19 @@ moment = Moment(app)
 class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators=[DataRequired()])
 	submit = SubmitField('Submit')
-	
-@app.route('/', methods=['GET', 'POST'])
-def index():
-	name = None
-	form = NameForm()
-	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
-		session['name'] = form.name.data
-		print("user_input:", form.name.data)
-		return redirect(url_for('index'))
-	return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
-	
-@app.route('/newbie')
+
+@app.route('/')
 def testing():
-		return render_template('base.html')
-	
+		return render_template('bootstrap.html')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
 
 
-	
-	
+
+
 if __name__ == "__main__":
 	#manager.run()
 	app.run(host='0.0.0.0', port=5010)
---
+++ Web/new project/app.py
@@ -0,0 +1,12 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def testing():
+		print( url_for('static', filename='bootstrap.css') )
+		return render_template("bootstrap.html")
+
+if __name__ == "__main__":
+	#manager.run()
+	app.run(host='0.0.0.0', port=5010)
