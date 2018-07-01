aad94f66b9f5498cf9a14c8af4f69972c1c461f6
perewodchik <perewodchik@tuta.io>
2018-07-01 13:04:40 +0300

    Ready for making features!

--- Web/new project/app.py
+++ Web/new project/app.py
@@ -1,11 +1,29 @@
 from flask import *
+from flask_session import Session
 
 app = Flask(__name__)
 
-@app.route('/')
-def testing():
-		print( url_for('static', filename='bootstrap.css') )
-		return render_template("bootstrap.html")
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+notes = []
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+	if request.method == "POST":
+		note = request.form.get("note")
+		notes.append(note)
+	return render_template("index.html", notes=notes)
+
+@app.route("/hello", methods=["GET", "POST"])
+def hello():
+	if request.method == "GET":
+		return "Please submit the form instead"
+	name = request.form.get("name")
+	return render_template("hello.html", name=name)
+
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
